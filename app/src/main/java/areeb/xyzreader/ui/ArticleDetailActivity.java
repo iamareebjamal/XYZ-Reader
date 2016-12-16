fc4bd9912608eab5ba92aaf65590a2ffd284161d
iamareebjamal <jamal.areeb@gmail.com>
2016-12-17 00:57:56 +0530

    Fixed CollapsingToolbar missing inset

--- app/src/main/java/areeb/xyzreader/ui/ArticleDetailActivity.java
+++ app/src/main/java/areeb/xyzreader/ui/ArticleDetailActivity.java
@@ -6,7 +6,10 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.OnApplyWindowInsetsListener;
+import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.view.WindowInsetsCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -39,11 +42,6 @@ public class ArticleDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().getDecorView().setSystemUiVisibility(
-                    View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
-        }
         setContentView(R.layout.activity_article_detail);
 
         articles = ArticleProvider.getArticles();
@@ -60,6 +58,23 @@ public class ArticleDetailActivity extends AppCompatActivity {
         mPager.setPageMargin((int) TypedValue
                 .applyDimension(TypedValue.COMPLEX_UNIT_DIP, 1, getResources().getDisplayMetrics()));
         mPager.setPageMarginDrawable(new ColorDrawable(0x22000000));
+
+        ViewCompat.setOnApplyWindowInsetsListener(mPager, new OnApplyWindowInsetsListener() {
+            @Override
+            public WindowInsetsCompat onApplyWindowInsets(View v, WindowInsetsCompat insets) {
+                insets = ViewCompat.onApplyWindowInsets(v, insets);
+                if(insets.isConsumed())
+                    return insets;
+
+                boolean consumed = false;
+                for(int i = 0, count = mPager.getChildCount(); i < count; i++) {
+                    ViewCompat.dispatchApplyWindowInsets(mPager.getChildAt(i), insets);
+                    if(insets.isConsumed())
+                        consumed = true;
+                }
+                return consumed ? insets.consumeSystemWindowInsets() : insets;
+            }
+        });
 
         if (savedInstanceState == null) {
             if (getIntent() != null && getIntent().hasExtra(ArticleDetailFragment.ARG_ITEM_ID)) {
